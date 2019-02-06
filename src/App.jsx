f18f5e0c0020776a7f434722ad4a3da48684e605
Ammar <ammar.alnatsheh@gmail.com>
2019-02-06 13:51:45 -0800

    init empty project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="app">
+       <h2>Tic Tac Toe</h2>
+       </div>
+    );
+  }
+}
+
+export default App;
